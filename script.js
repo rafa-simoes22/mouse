d7ded1dad6159eb5d486f80ef699e0f8bce38da2
Rafa <rafaelasimoes2203@gmail.com>
2023-10-09 22:27:55 -0300

    ajustes finais

---
+++ script.js
@@ -0,0 +1,14 @@
+function prox(){
+    var botao = document.getElementById("botaoo");
+    botao.addEventListener("click", location = '37.html');
+}
+
+function mensagem() {
+    var nome = document.getElementById("nome").value;
+    var sobrenome = document.getElementById("sobrenome").value;
+    var resposta = document.getElementById("resposta");
+    
+    if (nome && sobrenome) {
+        resposta.textContent = `Bom Trabalho ${nome} ${sobrenome}!`;
+    }
+}
